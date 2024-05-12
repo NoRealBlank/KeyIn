472d0d2663ce26696c55af156079d4d5fe90b1c7
Runze Zhang <742677062@qq.com>
2024-05-12 00:24:49 -0700

    v0

---
+++ models/lstm.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+class lstm(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, n_layers, batch_size):
+        super(lstm, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        self.n_layers = n_layers
+        self.embed = nn.Linear(input_size, hidden_size)
+        self.lstm = nn.ModuleList([nn.LSTMCell(hidden_size, hidden_size) for i in range(self.n_layers)])
+        self.output = nn.Sequential(
+                nn.Linear(hidden_size, output_size),
+                #nn.BatchNorm1d(output_size),
+                nn.Tanh())
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        hidden = []
+        for i in range(self.n_layers):
+            hidden.append((Variable(torch.zeros(self.batch_size, self.hidden_size).cuda()),
+                           Variable(torch.zeros(self.batch_size, self.hidden_size).cuda())))
+        return hidden
+    
+    def cond_hidden(self, cond):
+        hidden = []
+        for i in range(self.n_layers):
+            hidden.append((cond, cond))
+        return hidden
+
+    def forward(self, input):
+        embedded = self.embed(input.view(-1, self.input_size))
+        h_in = embedded
+
+        for i in range(self.n_layers):
+            self.hidden[i] = self.lstm[i](h_in, self.hidden[i])
+            h_in = self.hidden[i][0]
+
+        return self.output(h_in)
+
+class gaussian_lstm(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, n_layers, batch_size):
+        super(gaussian_lstm, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.batch_size = batch_size
+        self.embed = nn.Linear(input_size, hidden_size)
+        self.lstm = nn.ModuleList([nn.LSTMCell(hidden_size, hidden_size) for i in range(self.n_layers)])
+        self.mu_net = nn.Linear(hidden_size, output_size)
+        self.logvar_net = nn.Linear(hidden_size, output_size)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        hidden = []
+        for i in range(self.n_layers):
+            hidden.append((Variable(torch.zeros(self.batch_size, self.hidden_size).cuda()),
+                           Variable(torch.zeros(self.batch_size, self.hidden_size).cuda())))
+        return hidden
+
+    def reparameterize(self, mu, logvar):
+        logvar = logvar.mul(0.5).exp_()
+        eps = Variable(logvar.data.new(logvar.size()).normal_())
+        return eps.mul(logvar).add_(mu)
+
+    def forward(self, input):
+        embedded = self.embed(input.view(-1, self.input_size))
+        h_in = embedded
+        for i in range(self.n_layers):
+            self.hidden[i] = self.lstm[i](h_in, self.hidden[i])
+            h_in = self.hidden[i][0]
+        mu = self.mu_net(h_in)
+        logvar = self.logvar_net(h_in)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+
+
+import torch.nn.functional as F
+
+class EmbeddingMLP(nn.Module):
+    def __init__(self, input_size, hidden_sizes, output_size):
+        super(EmbeddingMLP, self).__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
+        for i in range(1, len(hidden_sizes)):
+            self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
+        self.layers.append(nn.Linear(hidden_sizes[-1], output_size))
+
+    def forward(self, x):
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+        x = self.layers[-1](x)
+        return x
+
+class KeyValueAttention(torch.nn.Module):
+    def __init__(self, key_dim, value_dim):
+        super(KeyValueAttention, self).__init__()
+        # Assume query_dim is the same as key_dim
+        self.scale = 1.0 / (key_dim ** 0.5)
+
+    def forward(self, query, keys, values):
+        """
+        query: Tensor of shape [batch_size, query_dim]
+        (embedding of the last keyframe)
+        
+        keys: Tensor of shape [batch_size, num_keys, key_dim]
+        (output embedding of inference network for all frames)
+        
+        values: Tensor of shape [batch_size, num_keys, value_dim]
+        (output value of the inference network for all frames)
+        """
+        # Compute the dot products
+        # query shape after unsqueeze: [batch_size, 1, query_dim]
+        # keys shape: [batch_size, num_keys, key_dim]
+        # scores shape: [batch_size, 1, num_keys]
+        scores = torch.matmul(query.unsqueeze(1), keys.transpose(-2, -1)) * self.scale
+
+        # Apply softmax to get attention weights
+        attn_weights = F.softmax(scores, dim=-1)
+
+        # Multiply the weights by the values
+        # attn_weights shape: [batch_size, 1, num_keys]
+        # values shape: [batch_size, num_keys, value_dim]
+        # output shape: [batch_size, 1, value_dim]
+        output = torch.matmul(attn_weights, values)
+
+        # Squeeze to remove the middle dimension
+        return output.squeeze(1)
---
+++ train_In.py
@@ -0,0 +1,457 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import argparse
+import os
+import random
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import utils
+import itertools
+import progressbar
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--lr', default=0.0002, type=float, help='learning rate')
+parser.add_argument('--beta1', default=0.9, type=float, help='momentum term for adam')
+parser.add_argument('--batch_size', default=100, type=int, help='batch size')
+parser.add_argument('--log_dir', default='logs/fp', help='base directory to save logs')
+parser.add_argument('--model_dir', default='', help='base directory to save logs')
+parser.add_argument('--name', default='', help='identifier for directory')
+parser.add_argument('--data_root', default='data', help='root directory for data')
+parser.add_argument('--optimizer', default='adam', help='optimizer to train with')
+parser.add_argument('--niter', type=int, default=300, help='number of epochs to train for')
+parser.add_argument('--seed', default=1, type=int, help='manual seed')
+parser.add_argument('--epoch_size', type=int, default=600, help='epoch size')
+parser.add_argument('--image_width', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--channels', default=1, type=int)
+parser.add_argument('--dataset', default='smmnist', help='dataset to train with')
+# parser.add_argument('--n_past', type=int, default=5, help='number of frames to condition on')
+# parser.add_argument('--n_future', type=int, default=10, help='number of frames to predict')
+# parser.add_argument('--n_eval', type=int, default=30, help='number of frames to predict at eval time')
+parser.add_argument('--rnn_size', type=int, default=256, help='dimensionality of hidden layer')
+# parser.add_argument('--posterior_rnn_layers', type=int, default=1, help='number of layers')
+parser.add_argument('--inpaintor_rnn_layers', type=int, default=2, help='number of layers')
+# parser.add_argument('--z_dim', type=int, default=10, help='dimensionality of z_t')
+parser.add_argument('--g_dim', type=int, default=128, help='dimensionality of encoder output vector and decoder input vector')
+# parser.add_argument('--beta', type=float, default=0.0001, help='weighting on KL to prior')
+parser.add_argument('--model', default='vgg', help='model type (dcgan | vgg)')
+parser.add_argument('--data_threads', type=int, default=5, help='number of data loading threads')
+parser.add_argument('--num_digits', type=int, default=2, help='number of digits for moving mnist')
+parser.add_argument('--last_frame_skip', action='store_true', help='if true, skip connections go between frame t and frame t+t rather than last ground truth frame')
+
+# dimensionality of delta
+parser.add_argument('--seg_length', type=int, default=10, help='max offsets between keyframes, which is one more than the max number of frames between keyframes.')
+
+
+opt = parser.parse_args()
+if opt.model_dir != '':
+    saved_model = torch.load('%s/model.pth' % opt.model_dir)
+    optimizer = opt.optimizer
+    model_dir = opt.model_dir
+    opt = saved_model['opt']
+    opt.optimizer = optimizer
+    opt.model_dir = model_dir
+    opt.log_dir = '%s/continued' % opt.log_dir
+else:
+    # name = 'model=%s%dx%d-rnn_size=%d-predictor-posterior-rnn_layers=%d-%d-n_past=%d-n_future=%d-lr=%.4f-g_dim=%d-z_dim=%d-last_frame_skip=%d-beta=%.7f%s' % (opt.model, opt.image_width, opt.image_width, opt.rnn_size, opt.predictor_rnn_layers, opt.posterior_rnn_layers, opt.n_past, opt.n_future, opt.lr, opt.g_dim, opt.z_dim, opt.last_frame_skip, opt.beta, opt.name)
+    # name = 'model=%s%dx%d-rnn_size=%d-inpaintor-rnn_layers=%d-n_past=%d-n_future=%d-lr=%.4f-g_dim=%d-last_frame_skip=%d-beta=%.7f-seg_length=%d%s' % (opt.model, opt.image_width, opt.image_width, opt.rnn_size, opt.inpaintor_rnn_layers, opt.n_past, opt.n_future, opt.lr, opt.g_dim, opt.last_frame_skip, opt.beta, opt.seg_length, opt.name)
+    name = 'model=%s%dx%d-rnn_size=%d-inpaintor-rnn_layers=%d-lr=%.4f-g_dim=%d-last_frame_skip=%d-seg_length=%d%s' % (opt.model, opt.image_width, opt.image_width, opt.rnn_size, opt.inpaintor_rnn_layers, opt.lr, opt.g_dim, opt.last_frame_skip, opt.seg_length, opt.name)
+    if opt.dataset == 'smmnist':
+        opt.log_dir = '%s/%s-%d/%s' % (opt.log_dir, opt.dataset, opt.num_digits, name)
+    else:
+        opt.log_dir = '%s/%s/%s' % (opt.log_dir, opt.dataset, name)
+
+os.makedirs('%s/gen/' % opt.log_dir, exist_ok=True)
+os.makedirs('%s/plots/' % opt.log_dir, exist_ok=True)
+
+print("Random Seed: ", opt.seed)
+random.seed(opt.seed)
+torch.manual_seed(opt.seed)
+torch.cuda.manual_seed_all(opt.seed)
+dtype = torch.cuda.FloatTensor
+
+
+# ---------------- load the models  ----------------
+print(opt)
+
+# ---------------- optimizers ----------------
+if opt.optimizer == 'adam':
+    opt.optimizer = optim.Adam
+elif opt.optimizer == 'rmsprop':
+    opt.optimizer = optim.RMSprop
+elif opt.optimizer == 'sgd':
+    opt.optimizer = optim.SGD
+else:
+    raise ValueError('Unknown optimizer: %s' % opt.optimizer)
+
+
+# ---------------- models ----------------
+import models.lstm as lstm_models
+
+# if opt.model_dir != '':
+#     frame_predictor = saved_model['frame_predictor']
+#     posterior = saved_model['posterior']
+# else:
+#     frame_predictor = lstm_models.lstm(opt.g_dim+opt.z_dim, opt.g_dim, opt.rnn_size, opt.predictor_rnn_layers, opt.batch_size)
+#     posterior = lstm_models.gaussian_lstm(opt.g_dim, opt.z_dim, opt.rnn_size, opt.posterior_rnn_layers, opt.batch_size)
+#     frame_predictor.apply(utils.init_weights)
+#     posterior.apply(utils.init_weights)
+
+if opt.model_dir != '':
+    embedder = saved_model['embedder']
+    inpaintor = saved_model['inpaintor']
+else:
+    embedder = lstm_models.EmbeddingMLP(2*opt.g_dim + opt.seg_length, [2*opt.rnn_size]*4, opt.rnn_size)
+    inpaintor = lstm_models.lstm(opt.g_dim, opt.g_dim, opt.rnn_size, opt.inpaintor_rnn_layers, opt.batch_size)
+    embedder.apply(utils.init_weights)
+    inpaintor.apply(utils.init_weights)
+
+
+if opt.model == 'dcgan':
+    if opt.image_width == 64:
+        import models.dcgan_64 as model 
+    elif opt.image_width == 128:
+        import models.dcgan_128 as model  
+elif opt.model == 'vgg':
+    if opt.image_width == 64:
+        import models.vgg_64 as model
+    elif opt.image_width == 128:
+        import models.vgg_128 as model
+else:
+    raise ValueError('Unknown model: %s' % opt.model)
+        
+if opt.model_dir != '':
+    decoder = saved_model['decoder']
+    encoder = saved_model['encoder']
+else:
+    encoder = model.encoder(opt.g_dim, opt.channels)
+    decoder = model.decoder(opt.g_dim, opt.channels)
+    encoder.apply(utils.init_weights)
+    decoder.apply(utils.init_weights)
+
+
+# frame_predictor_optimizer = opt.optimizer(frame_predictor.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+# posterior_optimizer = opt.optimizer(posterior.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+embedder_optimizer = opt.optimizer(embedder.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+inpaintor_optimizer = opt.optimizer(inpaintor.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+encoder_optimizer = opt.optimizer(encoder.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+decoder_optimizer = opt.optimizer(decoder.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
+
+# --------- loss functions ------------------------------------
+# mse_criterion = nn.MSELoss()
+# def kl_criterion(mu, logvar):
+#   # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+#   KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+#   KLD /= opt.batch_size  
+#   return KLD
+bce_criterion = nn.BCELoss()
+
+
+# --------- transfer to gpu ------------------------------------
+# frame_predictor.cuda()
+# posterior.cuda()
+embedder.cuda()
+inpaintor.cuda()
+encoder.cuda()
+decoder.cuda()
+# mse_criterion.cuda()
+bce_criterion.cuda()
+
+# --------- load a dataset ------------------------------------
+train_data, test_data = utils.load_dataset(opt)
+
+train_loader = DataLoader(train_data,
+                          num_workers=opt.data_threads,
+                          batch_size=opt.batch_size,
+                          shuffle=True,
+                          drop_last=True,
+                          pin_memory=True)
+test_loader = DataLoader(test_data,
+                         num_workers=opt.data_threads,
+                         batch_size=opt.batch_size,
+                         shuffle=True,
+                         drop_last=True,
+                         pin_memory=True)
+
+def get_training_batch():
+    while True:
+        for sequence in train_loader:
+            batch = utils.normalize_data(opt, dtype, sequence)
+            yield batch
+training_batch_generator = get_training_batch()
+
+def get_testing_batch():
+    while True:
+        for sequence in test_loader:
+            batch = utils.normalize_data(opt, dtype, sequence)
+            yield batch 
+testing_batch_generator = get_testing_batch()
+
+# --------- plotting funtions ------------------------------------
+def plot(x, epoch):
+    nsample = 1 
+    gen_seq = [[] for i in range(nsample)]
+    gt_seq = [x[i] for i in range(len(x))]
+
+    len_in = random.randint(3, 8)
+    delta_key = []
+    for i in range(opt.seg_length):
+        if i == len_in-2:
+            delta_key.append(0.8)
+        else:
+            delta_key.append(0.2/opt.seg_length)
+    delta_key = torch.tensor(delta_key).unsqueeze(0).repeat(opt.batch_size, 1).cuda()
+
+    # h_seq = [encoder(x[i]) for i in range(opt.n_past)]
+
+    h_seq = [encoder(x[i]) for i in range(len_in)]
+    h_cond = torch.cat([h_seq[0][0], h_seq[len_in-1][0], delta_key], 1)
+
+    for s in range(nsample):
+        # frame_predictor.hidden = frame_predictor.init_hidden()
+        inpaintor.hidden = inpaintor.cond_hidden(embedder(h_cond))
+        gen_seq[s].append(x[0])
+        x_in = x[0]
+        for i in range(1, len_in-1):
+            # if opt.last_frame_skip or i < opt.n_past:	
+            #     h, skip = h_seq[i-1]
+            #     h = h.detach()
+            # elif i < opt.n_past:
+            #     h, _ = h_seq[i-1]
+            #     h = h.detach()
+            if opt.last_frame_skip or i == 1:
+                h, skip = h_seq[i-1]
+                h = h.detach()
+
+            # if i < opt.n_past:
+            #     z_t, _, _ = posterior(h_seq[i][0])
+            #     frame_predictor(torch.cat([h, z_t], 1)) 
+            #     x_in = x[i]
+            #     gen_seq[s].append(x_in)
+            # else:
+            #     z_t = torch.cuda.FloatTensor(opt.batch_size, opt.z_dim).normal_()
+            #     h = frame_predictor(torch.cat([h, z_t], 1)).detach()
+            #     x_in = decoder([h, skip]).detach()
+            #     gen_seq[s].append(x_in)
+
+            h = inpaintor(h).detach()
+            x_in = decoder([h, skip]).detach()
+            gen_seq[s].append(x_in)
+        
+        gen_seq[s].append(x[len_in-1])
+
+
+    to_plot = []
+    gifs = [ [] for t in range(len_in) ]
+    nrow = min(opt.batch_size, 10)
+    for i in range(nrow):
+        # ground truth sequence
+        row = [] 
+        for t in range(len_in):
+            row.append(gt_seq[t][i])
+        to_plot.append(row)
+
+        for s in range(nsample):
+            row = []
+            for t in range(len_in):
+                row.append(gen_seq[s][t][i]) 
+            to_plot.append(row)
+
+        for t in range(len_in):
+            row = []
+            row.append(gt_seq[t][i])
+            for s in range(nsample):
+                row.append(gen_seq[s][t][i])
+            gifs[t].append(row)
+
+    fname = '%s/gen/sample_%d.png' % (opt.log_dir, epoch) 
+    utils.save_tensors_image(fname, to_plot)
+
+    # fname = '%s/gen/sample_%d.gif' % (opt.log_dir, epoch) 
+    # utils.save_gif(fname, gifs)
+
+
+def plot_rec(x, epoch):
+
+    len_in = random.randint(3, 8)
+    delta_key = []
+    for i in range(opt.seg_length):
+        if i == len_in-2:
+            delta_key.append(0.8)
+        else:
+            delta_key.append(0.2/opt.seg_length)
+    delta_key = torch.tensor(delta_key).unsqueeze(0).repeat(opt.batch_size, 1).cuda()
+
+    # frame_predictor.hidden = frame_predictor.init_hidden()
+    # posterior.hidden = posterior.init_hidden()
+    # inpaintor.hidden = inpaintor.init_hidden()
+
+    # h_seq = [encoder(x[i]) for i in range(opt.n_past+opt.n_future)]
+    h_seq = [encoder(x[i]) for i in range(len_in)]
+    h_cond = torch.cat([h_seq[0][0], h_seq[len_in-1][0], delta_key], 1)
+    inpaintor.hidden = inpaintor.cond_hidden(embedder(h_cond))
+
+    gen_seq = []
+    gen_seq.append(x[0])
+    # x_in = x[0]
+    for i in range(1, len_in-1):
+        # h_target = h_seq[i][0].detach()
+        # if opt.last_frame_skip or i < opt.n_past:	
+        #     h, skip = h_seq[i-1]
+        # else:
+        #     h, _ = h_seq[i-1]
+        # h = h.detach()
+        if opt.last_frame_skip or i == 1:
+            h, skip = h_seq[i-1]
+        else:
+            h = h_seq[i-1][0]
+        h = h.detach()
+
+        # z_t, mu, logvar = posterior(h_target)
+        # if i < opt.n_past:
+        #     frame_predictor(torch.cat([h, z_t], 1)) 
+        #     gen_seq.append(x[i])
+        # else:
+        #     h_pred = frame_predictor(torch.cat([h, z_t], 1)).detach()
+        #     x_pred = decoder([h_pred, skip]).detach()
+        #     gen_seq.append(x_pred)
+        h_pred = inpaintor(h).detach()
+        x_pred = decoder([h_pred, skip]).detach()
+        gen_seq.append(x_pred)
+    gen_seq.append(x[len_in-1])
+   
+    to_plot = []
+    nrow = min(opt.batch_size, 10)
+    for i in range(nrow):
+        row = []
+        for t in range(len_in):
+            row.append(gen_seq[t][i]) 
+        to_plot.append(row)
+    fname = '%s/gen/rec_%d.png' % (opt.log_dir, epoch) 
+    utils.save_tensors_image(fname, to_plot)
+
+
+# --------- training funtions ------------------------------------
+def train(x):
+    # frame_predictor.zero_grad()
+    # posterior.zero_grad()
+    embedder.zero_grad()
+    inpaintor.zero_grad()
+    encoder.zero_grad()
+    decoder.zero_grad()
+
+    # initialize the hidden state.
+    # frame_predictor.hidden = frame_predictor.init_hidden()
+    # posterior.hidden = posterior.init_hidden()
+    # inpaintor.hidden = inpaintor.init_hidden()
+
+    len_in = random.randint(3, 8)
+
+    delta_key = []
+    for i in range(opt.seg_length):
+        if i == len_in-2:
+            delta_key.append(0.8)
+        else:
+            delta_key.append(0.2/opt.seg_length)
+    delta_key = torch.tensor(delta_key).unsqueeze(0).repeat(opt.batch_size, 1).cuda()
+    
+    h_seq = [encoder(x[i]) for i in range(len_in)]
+    h_cond = torch.cat([h_seq[0][0], h_seq[len_in-1][0], delta_key], 1)
+    inpaintor.hidden = inpaintor.cond_hidden(embedder(h_cond))
+
+    bce = 0
+    for i in range(1, len_in-1):
+        # h_target = h_seq[i][0]
+        # if opt.last_frame_skip or i < opt.n_past:	
+        #     h, skip = h_seq[i-1]
+        # else:
+        #     h = h_seq[i-1][0]
+        if opt.last_frame_skip or i == 1:	
+            h, skip = h_seq[i-1]
+        else:
+            h = h_seq[i-1][0]
+        
+        # z_t, mu, logvar = posterior(h_target)
+        # h_pred = frame_predictor(torch.cat([h, z_t], 1))
+        h_pred = inpaintor(h)
+        x_pred = decoder([h_pred, skip])
+
+        # mse += mse_criterion(x_pred, x[i])
+        # kld += kl_criterion(mu, logvar)
+
+        # need to change the loss according to the paper
+        bce += bce_criterion(x_pred, x[i])
+
+    # loss = mse + kld*opt.beta
+    loss = bce
+    loss.backward()
+
+    # frame_predictor_optimizer.step()
+    # posterior_optimizer.step()
+    embedder_optimizer.step()
+    inpaintor_optimizer.step()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    # return mse.data.cpu().numpy()/(opt.n_past+opt.n_future), kld.data.cpu().numpy()/(opt.n_future+opt.n_past)
+    return bce.data.cpu().numpy()/(len_in-2)
+
+
+# --------- training loop ------------------------------------
+for epoch in range(opt.niter):
+    # frame_predictor.train()
+    # posterior.train()
+    embedder.train()
+    inpaintor.train()
+    encoder.train()
+    decoder.train()
+    # epoch_mse = 0
+    # epoch_kld = 0
+    epoch_bce = 0
+    progress = progressbar.ProgressBar(max_value=opt.epoch_size).start()
+    for i in range(opt.epoch_size):
+        progress.update(i+1)
+        x = next(training_batch_generator)
+
+        # train frame_predictor 
+        # mse, kld = train(x)
+        # epoch_mse += mse
+        # epoch_kld += kld
+        bce = train(x)
+        epoch_bce += bce
+
+
+    progress.finish()
+    utils.clear_progressbar()
+
+    # print('[%02d] mse loss: %.5f | kld loss: %.5f (%d)' % (epoch, epoch_mse/opt.epoch_size, epoch_kld/opt.epoch_size, epoch*opt.epoch_size*opt.batch_size))
+    print('[%02d] bce loss: %.5f (%d)' % (epoch, epoch_bce/opt.epoch_size, epoch*opt.epoch_size*opt.batch_size))
+
+    # plot some stuff
+    # frame_predictor.eval()
+    embedder.eval()
+    inpaintor.eval()
+    encoder.eval()
+    decoder.eval()
+    # posterior.eval()
+    x = next(testing_batch_generator)
+    plot(x, epoch)
+    plot_rec(x, epoch)
+
+    # save the model
+    torch.save({
+        'encoder': encoder,
+        'decoder': decoder,
+        # 'frame_predictor': frame_predictor,
+        # 'posterior': posterior,
+        'embedder': embedder,
+        'inpaintor': inpaintor,
+        'opt': opt},
+        '%s/model.pth' % opt.log_dir)
+    
+    if (epoch + 1) % 10 == 0:
+        print('log dir: %s' % opt.log_dir)
+        
+
